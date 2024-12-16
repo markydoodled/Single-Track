171dc2b2f576db54e76275b016ce76294c02d774
Mark Howard <markhoward2005@gmail.com>
2024-12-16 20:03:23 +0000

    Uploaded Source Code

---
+++ Source Code/Single Track/Single Track/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Single Track
+//
+//  Created by Mark Howard on 09/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Source Code/Single Track/Single Track/Single_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Single_TrackApp.swift
+//  Single Track
+//
+//  Created by Mark Howard on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Single_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
